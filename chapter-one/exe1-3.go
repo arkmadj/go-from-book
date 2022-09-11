29a8a926d15a60fd502449d6fdf1af678c9b0a5a
arkmadj <ahmadjinadu@gmail.com>
2022-09-11 14:58:04 +0100

    Added dup and nanoseconds

--- chapter-one/exe1-3.go
+++ chapter-one/exe1-3.go
@@ -10,19 +10,19 @@ import (
 func echo1() {
 	start := time.Now()
 	var s, sep string
-	for _, arg := range os.Args[:] {
+	for _, arg := range os.Args[1:] {
 		s += sep + arg
 		sep = " "
 	}
-	fmt.Printf("%s %.2fs \n", s, time.Since(start).Seconds())
+	fmt.Printf("%s %vs \n", s, time.Since(start).Nanoseconds())
 }
 
 func echo3() {
 	start := time.Now()
-	fmt.Printf("%s %.2fs \n", strings.Join(os.Args[:], " "), time.Since(start).Seconds())
+	fmt.Printf("%s %.vs \n", strings.Join(os.Args[1:], " "), time.Since(start).Nanoseconds())
 }
 
 func main() {
-	go echo1()
-	go echo3()
+	echo1()
+	echo3()
 }
